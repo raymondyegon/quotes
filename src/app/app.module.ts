afc053f1154cceb98c787a1c7a60637aeae6199b
Raymond Yegon <raymondyegon17@gmail.com>
2019-03-24 17:17:14 +0300

    Post form directive added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { PostDirective } from './post.directive';
+import { PostFormComponent } from './post-form/post-form.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    PostDirective,
+    PostFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/post.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PostDirective } from './post.directive';
+
+describe('PostDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PostDirective();
+    expect(directive).toBeTruthy();
+  });
+});
