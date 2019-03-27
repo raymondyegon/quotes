bfd83bbaed425acb0233490b9b3f785998a0ec46
Raymond Yegon <raymondyegon17@gmail.com>
2019-03-27 10:50:20 +0300

    Upload image sector updated

---
+++ src/app/upload-image/upload-image.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-image',
+  templateUrl: './upload-image.component.html',
+  styleUrls: ['./upload-image.component.css']
+})
+export class UploadImageComponent implements OnInit {
+  public imagePath;
+  imageURL; any;
+  public message: string;
+
+  preview(files) {
+    if (files.length === 0)
+      return;
+
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsArrayBuffer(files[0]);
+    reader.onload = (_event) => {
+      this.imageURL = reader.result;
+    }
+  }
+
+  onUpload(){
+
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
