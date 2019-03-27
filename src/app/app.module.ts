44e8fccc0f446cd95c5c2352feebd7938114dfe7
Raymond Yegon <raymondyegon17@gmail.com>
2019-03-27 11:42:56 +0300

    ready for deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { PostDirective } from './post.directive';
 import { PostFormComponent } from './post-form/post-form.component';
 import { from } from 'rxjs';
+import { UploadImageComponent } from './upload-image/upload-image.component';
+import { VoteComponent } from './vote/vote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     PostDirective,
-    PostFormComponent
+    PostFormComponent,
+    UploadImageComponent,
+    VoteComponent
   ],
   imports: [
     BrowserModule,
